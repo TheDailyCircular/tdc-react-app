a0fc65d5b43622e69e7d6d8d2b1cb8100d428fd9
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-07-23 02:28:26 +0600

    Basic routes and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Posts from './component/posts/Posts'
+import Messages from './component/message/Messages';
+import Circulars from './component/circular/Circulars';
+import UserProfile from './component/user/UserProfile';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Posts} />
+        <Route exact path="/posts" component={Posts} />
+        <Route exact path="/message" component={Messages} />
+        <Route exact path="/circular" component={Circulars} />
+        <Route exact path="/profile" component={UserProfile} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/circular/Circulars.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Circulars extends Component {
+  render() {
+    return (
+      <div>
+        Circulars
+      </div>
+    )
+  }
+}
+
+export default Circulars;
