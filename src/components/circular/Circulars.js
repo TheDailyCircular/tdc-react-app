682f3bb38455546b17a3e91cbb06ca3700958b1e
kmtusher97 <kmtusher97@gmail.com>
2020-08-19 21:18:33 +0600

    Updated Circulars component add circular reducer fetching state

--- src/components/circular/Circulars.js
+++ src/components/circular/Circulars.js
@@ -1,6 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Circulars.css';
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { getCirculars } from '../../actions/CircularActions';
 
 class Circulars extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pageNo: 0
+    }
+  }
+
+  componentDidMount = () => {
+    console.log("pageno " + this.state.pageNo);
+    this.props.getCirculars(this.state.pageNo);
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +26,17 @@ class Circulars extends Component {
   }
 }
 
-export default Circulars;
+Circulars.protoType = {
+  circulars: PropTypes.object.isRequired,
+  getCirculars: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  circulars: state.circular.circulars
+});
+
+const mapDispatchToProps = {
+  getCirculars: getCirculars
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Circulars);
