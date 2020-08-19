6e30f16012f1e6ed9bf1587d12cd951228ac069b
kmtusher97 <kmtusher97@gmail.com>
2020-08-20 01:15:41 +0600

    Create Circular form

--- src/components/circular/CreateCircular.js
+++ src/components/circular/CreateCircular.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import { Container, Form, Button } from 'react-bootstrap';
 
 class CreateCircular extends Component {
+  constructor() {
+    super();
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit = event => { }
+
   render() {
     return (
-      <div>
-        CreateCircular
-      </div>
+      <Container id="create-circular-container">
+        <Form id="create-circular-from" onSubmit={this.onSubmit}>
+          <Form.Group>
+            <Form.Label>Circular Title</Form.Label>
+            <Form.Control
+              name="title"
+              type="text"
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Expiration Date</Form.Label>
+            <Form.Control
+              name="expirationDate"
+              type="date"
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Circular Body</Form.Label>
+            <textarea
+              className="form-control"
+              name="text"
+            >
+            </textarea>
+          </Form.Group>
+          <Form.Group>
+            <Button className="form-control" type="submit">Post</Button>
+          </Form.Group>
+        </Form>
+      </Container>
     )
   }
 }
